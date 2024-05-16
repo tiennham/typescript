fc794b916c2ca7abc4d302b7433b60c81ff6aeb5
Tien Nham <tiennham@Tiens-MacBook-Pro.local>
2024-05-16 18:35:20 +0700

    void type: not return

---
+++ project/lesson16/lesson.ts
@@ -0,0 +1,11 @@
+const sum = (a: number, b: number): number => {
+    return a + b
+}
+
+sum("aa", "bb")
+
+const handleLog = (message: string): void => {
+    console.log(" my message: ", message)
+}
+
+export { }
---
+++ project/lesson16/lesson16.js
@@ -0,0 +1,3 @@
+let name = "Tien"
+name = 12
+name = true
