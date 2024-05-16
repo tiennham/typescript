3362d5181dce10c560aa763b4e433c1d8edac775
Tien Nham <tiennham@Tiens-MacBook-Pro.local>
2024-05-16 10:24:41 +0700

    Demontrade adding number in array of string

---
+++ project/lesson6/lesson6.js
@@ -0,0 +1,9 @@
+
+// node project/lesson6/lesson6.js
+
+let string_arr = ["TypeScript", "NodeJS", "AWS"]
+
+string_arr.push(6) // OK
+
+console.log("string array after push integer: ", string_arr) // [ 'TypeScript', 'NodeJS', 'AWS', 6 ]
+
