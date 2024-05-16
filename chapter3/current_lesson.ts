95b8b63d0be7df8c86dd19f6aa68fc436ebcb274
Tien Nham <tiennham@Tiens-MacBook-Pro.local>
2024-05-16 23:44:24 +0700

    if else

---
+++ chapter3/current_lesson.ts
@@ -0,0 +1,15 @@
+// lesson 19
+
+// let age = 18
+// let age = 15
+let age = 10
+
+if (age >= 18) {
+    console.log("you can watch JAV")
+} else if (age >= 15) {
+    console.log("You can watch advanture")
+} else {
+    console.log("You can watch cartoon")
+}
+
+export { }
