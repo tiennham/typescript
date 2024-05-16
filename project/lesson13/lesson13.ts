839587f8d10ba71c37b53c53e16566878b46f29c
Tien Nham <tiennham@Tiens-MacBook-Pro.local>
2024-05-16 16:33:05 +0700

    tuple:

---
+++ project/lesson13/lesson13.ts
@@ -0,0 +1,15 @@
+// tuple
+
+let student_tuple: [string, number, boolean];
+
+student_tuple = ["Jane", 20, true]
+
+console.log("studen tuple", student_tuple)
+
+let student_tuple_optional: [string, number, boolean?];
+
+student_tuple_optional = ["Harry", 21]
+
+console.log("student_tuple_optional: ", student_tuple_optional)
+
+export { }
