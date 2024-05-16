0134c99565d66286f454f810aebef37ff9f0878c
Tien Nham <tiennham@Tiens-MacBook-Pro.local>
2024-05-16 18:37:10 +0700

    fix lesson16

--- project/lesson16/lesson.ts
+++ project/lesson16/lesson.ts
@@ -2,8 +2,6 @@ const sum = (a: number, b: number): number => {
     return a + b
 }
 
-sum("aa", "bb")
-
 const handleLog = (message: string): void => {
     console.log(" my message: ", message)
 }
