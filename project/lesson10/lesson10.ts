395c080cda744a278398f4258525a87d85d4082a
Tien Nham <tiennham@Tiens-MacBook-Pro.local>
2024-05-16 12:50:04 +0700

    boolean with Boolean class

---
+++ project/lesson10/lesson10.ts
@@ -0,0 +1,16 @@
+// ts-node project/lesson10/lesson10.ts
+
+
+let boolean_true: boolean = true
+let boolean_false = false
+let boolean_true_obj: boolean = Boolean("Nham Hong Tien")
+let boolean_false_obj: boolean = Boolean("")
+let boolean_false_obj_2: boolean = Boolean(0)
+
+console.log('boolean_true: ', boolean_true)
+console.log('boolean_false: ', boolean_false)
+console.log('boolean_true_obj: ', boolean_true_obj)
+console.log('boolean_false_obj: ', boolean_false_obj)
+console.log('boolean_false_obj_2: ', boolean_false_obj_2)
+
+export { } 
