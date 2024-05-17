53700d37baed90900fdbb9006384e42043550061
Tien Nham <tiennham@Tiens-MacBook-Pro.local>
2024-05-17 09:02:51 +0700

    while loop: when you wanna break inside the body of while loop

--- chapter3/current_lesson.ts
+++ chapter3/current_lesson.ts
@@ -1,35 +1,17 @@
-// lesson 22: for loop 
-console.log(`\n`)
-console.log("//==== Normal case ====")
-for (let i = 0; i < 10; i++) {
-    console.log(">>> i: ", i)
-}
-
-console.log(`\n`)
-console.log("//==== Missing initialize case ====")
-let i2 = 0
-for (; i2 < 10; i2++) {
-    console.log(">>> i2: ", i2)
-}
-
-console.log(`\n`)
-console.log("//==== Missing initialize and break point case ====")
-let i3 = 0
-for (; ; i3++) {
-    if (i3 >= 10) {
+// lesson 23: while loop 
+let n = 1
+while (true) {
+    console.log(`\n`)
+    console.log(`//==== Throw the dices ${n} times ====`)
+    let dice1 = Math.floor(Math.random() * 6) + 1
+    console.log(`Dice 1: ${dice1}`)
+    let dice2 = Math.floor(Math.random() * 6) + 1
+    console.log(`Dice 2: ${dice2}`)
+    if (dice1 == dice2) { // This is the purpose for while loop, break in the body
+        console.log(`Bye loop`)
         break
     }
-    console.log(">>> i3: ", i3)
-}
-console.log(`\n`)
-console.log("//==== Missing all case ====")
-let i4 = 0
-for (; ;) {
-    i4++
-    if (i4 >= 10) {
-        break
-    }
-    console.log(">>> i4: ", i4)
+    n++
 }
 
 export { }
