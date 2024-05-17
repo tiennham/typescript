1d0e702d39fb29c1bce3d24bd21ea91baf24641d
Tien Nham <tiennham@Tiens-MacBook-Pro.local>
2024-05-17 08:33:09 +0700

    for loop

--- chapter3/current_lesson.ts
+++ chapter3/current_lesson.ts
@@ -1,23 +1,35 @@
-// lesson 21: switch case control
+// lesson 22: for loop 
+console.log(`\n`)
+console.log("//==== Normal case ====")
+for (let i = 0; i < 10; i++) {
+    console.log(">>> i: ", i)
+}
 
-let age = Math.floor(Math.random() * 18) + 1
-// let age = 17
-console.log(">>> age: ", age)
+console.log(`\n`)
+console.log("//==== Missing initialize case ====")
+let i2 = 0
+for (; i2 < 10; i2++) {
+    console.log(">>> i2: ", i2)
+}
 
-switch (age) {
-    case 18:
-    case 17:
-        console.log(`age ${age} is High schood graduate`)
-        break;
-    case 15:
-        console.log(`age ${age} is Secondary school graduate`)
-        break;
-    case 11:
-        console.log(`age ${age} is Elementary school graduate`)
-        break;
-    default:
-        console.log(`No worry, age ${age} just play all the time!!!`);
-        break;
+console.log(`\n`)
+console.log("//==== Missing initialize and break point case ====")
+let i3 = 0
+for (; ; i3++) {
+    if (i3 >= 10) {
+        break
+    }
+    console.log(">>> i3: ", i3)
+}
+console.log(`\n`)
+console.log("//==== Missing all case ====")
+let i4 = 0
+for (; ;) {
+    i4++
+    if (i4 >= 10) {
+        break
+    }
+    console.log(">>> i4: ", i4)
 }
 
 export { }
