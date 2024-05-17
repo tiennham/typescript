887012b896f134d45c3aa4caff1e9526f46199b5
Tien Nham <tiennham@Tiens-MacBook-Pro.local>
2024-05-17 09:09:40 +0700

    do while loop

--- chapter3/backup_lesson.ts
+++ chapter3/backup_lesson.ts
@@ -92,3 +92,27 @@ while (true) {
 }
 
 export { }
+
+
+// lesson 24: do while loop 
+let day = Math.floor(Math.random() * 7) + 1
+let n = 1
+do {
+    console.log(`\n`)
+    console.log(`//==== Throw the dices ${n} times ====`)
+    if (day == 7) {
+        console.log(`Today is Sunday, let take a break.`)
+        break
+    }
+    let dice1 = Math.floor(Math.random() * 6) + 1
+    console.log(`Dice 1: ${dice1}`)
+    let dice2 = Math.floor(Math.random() * 6) + 1
+    console.log(`Dice 2: ${dice2}`)
+    if (dice1 == dice2) { // This is the purpose for while loop, break in the body
+        console.log(`Bye loop`)
+        break
+    }
+    n++
+} while (true)
+
+export { }
--- chapter3/current_lesson.ts
+++ chapter3/current_lesson.ts
@@ -1,8 +1,13 @@
-// lesson 23: while loop 
+// lesson 24: do while loop 
+let day = Math.floor(Math.random() * 7) + 1
 let n = 1
-while (true) {
+do {
     console.log(`\n`)
     console.log(`//==== Throw the dices ${n} times ====`)
+    if (day == 7) {
+        console.log(`Today is Sunday, let take a break.`)
+        break
+    }
     let dice1 = Math.floor(Math.random() * 6) + 1
     console.log(`Dice 1: ${dice1}`)
     let dice2 = Math.floor(Math.random() * 6) + 1
@@ -12,6 +17,6 @@ while (true) {
         break
     }
     n++
-}
+} while (true)
 
 export { }
