2bd5c2757a3019f5f0f0f76b0cd6d0d32f1f5e74
Tien Nham <tiennham@Tiens-MacBook-Pro.local>
2024-05-17 12:03:26 +0700

    function normal and arrow function

--- chapter3/backup_lesson.ts
+++ chapter3/backup_lesson.ts
@@ -208,3 +208,35 @@ for (let i = 0; i < team_guys.length; i++) {
 }
 
 export { }
+
+
+// lesson 27: function
+
+// case normal
+
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+console.log("sum function normal: ", sum(5, 1))
+
+// arrow function
+// https://viblo.asia/p/su-khac-biet-giua-arrow-function-va-function-trong-javascript-07LKXpw2KV4
+// khong nen dung arrow function trong phuong thuc cua object, 
+// vi arrow fucntion khong co bind & this thay doi kho kiem soat
+// dung trong cac ham filter, map cua array thi tien dung, ngan gon hon
+// arrow function co the khong can return
+
+const sum_arrow = (a: number, b: number) => a + b
+
+console.log("arrow function: ", sum_arrow(22, 33))
+
+
+let init_array = [2, 3, 4, 5, 6]
+
+let double_array = init_array.map(item => item * 2)
+
+console.log("Init array: ", init_array)
+console.log("Double array: ", double_array)
+
+export { }
--- chapter3/current_lesson.ts
+++ chapter3/current_lesson.ts
@@ -1,14 +1,30 @@
-// lesson 26: continue keyword
+// lesson 27: function
 
-let ignore_guy = "cung dau"
+// case normal
 
-let team_guys = ["ninh than", "cung dau", "xinh dep", "culi", "leader"]
-
-for (let i = 0; i < team_guys.length; i++) {
-    if (team_guys[i] == 'cung dau') {
-        continue
-    }
-    console.log(`${team_guys[i]}, You have 1000$ for your efforce!!`)
+function sum(a: number, b: number) {
+    return a + b;
 }
+
+console.log("sum function normal: ", sum(5, 1))
+
+// arrow function
+// https://viblo.asia/p/su-khac-biet-giua-arrow-function-va-function-trong-javascript-07LKXpw2KV4
+// khong nen dung arrow function trong phuong thuc cua object, 
+// vi arrow fucntion khong co bind & this thay doi kho kiem soat
+// dung trong cac ham filter, map cua array thi tien dung, ngan gon hon
+// arrow function co the khong can return
+
+const sum_arrow = (a: number, b: number) => a + b
+
+console.log("arrow function: ", sum_arrow(22, 33))
+
+
+let init_array = [2, 3, 4, 5, 6]
+
+let double_array = init_array.map(item => item * 2)
+
+console.log("Init array: ", init_array)
+console.log("Double array: ", double_array)
 
 export { }
