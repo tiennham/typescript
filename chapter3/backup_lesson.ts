4d02c954a5de9ab2206efd9a45b5374d88a6d222
Tien Nham <tiennham@Tiens-MacBook-Pro.local>
2024-05-18 11:27:35 +0700

    class

--- chapter3/backup_lesson.ts
+++ chapter3/backup_lesson.ts
@@ -413,3 +413,30 @@ count_obj.count(10) // this.current = 3, 4, 5, 6, 7, 8, 9, 10
 export { }
 
 
+// lesson 32: class
+
+class Person {
+    ssn: string;
+    firstName: string;
+    lastName: string;
+
+    constructor(ssn: string, first_name: string, last_name: string) {
+        this.firstName = first_name
+        this.lastName = last_name
+        this.ssn = ssn
+    }
+
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+let person = new Person("123", "Tien", "Nham")
+
+console.log(">>> person: ", person)
+console.log(">>> person.getFullName: ", person.getFullName())
+
+
+export { }
+
+
--- chapter3/current_lesson.ts
+++ chapter3/current_lesson.ts
@@ -1,50 +1,25 @@
-// lesson 31: overload function
+// lesson 32: class
 
-// We want to combine addNumber and concateString
+class Person {
+    ssn: string;
+    firstName: string;
+    lastName: string;
 
-function addNumber(a: number, b: number): number {
-    return a + b
-}
+    constructor(ssn: string, first_name: string, last_name: string) {
+        this.firstName = first_name
+        this.lastName = last_name
+        this.ssn = ssn
+    }
 
-function concateString(a: string, b: string): string {
-    return a + b
-}
-
-// And here is how we do
-function addOrConcate(a: number, b: number): number;
-
-function addOrConcate(a: string, b: string): string;
-
-function addOrConcate(a: any, b: any): number | string {
-    return a + b
-}
-
-console.log(">>> check addOrConcate: ", addOrConcate(6, 9), addOrConcate("Tien", " & TypeScrip"))
-
-
-class CountThing {
-    private current: number = 0;
-    count(): number;
-    count(target: number): number[];
-    count(target?: number): number | number[] {
-        if (target) {
-            let values: number[] = []
-            for (let i = this.current; i <= target; i++) {
-                // console.log(">>> i: ", i)
-                values.push(i)
-            }
-            console.log("Values: ", values)
-            return values
-        }
-        console.log("increase 1 for this.current: ", this.current)
-        return ++this.current
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`
     }
 }
 
-let count_obj = new CountThing();
-count_obj.count() // this.current = 0
-count_obj.count() // this.current = 1
-count_obj.count() // this.current = 2
-count_obj.count(10) // this.current = 3, 4, 5, 6, 7, 8, 9, 10
+let person = new Person("123", "Tien", "Nham")
+
+console.log(">>> person: ", person)
+console.log(">>> person.getFullName: ", person.getFullName())
+
 
 export { }
