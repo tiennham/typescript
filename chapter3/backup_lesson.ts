1b4e9eec45a6295cf602549ce43c4c0f955a0c09
Tien Nham <tiennham@Tiens-MacBook-Pro.local>
2024-05-18 08:25:06 +0700

    default parameter

--- chapter3/backup_lesson.ts
+++ chapter3/backup_lesson.ts
@@ -278,3 +278,19 @@ console.log(">>> sum(6, 7): ", sum(6, 7))
 // console.log(">>> sum(): ", sum()) // error
 
 export { }
+
+
+// lesson 30: default parameter
+
+function sum(a: number, b: number, c = false) {
+    if (c === false) {
+        return a - b
+    }
+    return a + b
+}
+
+console.log(">>> sum(6, 7): ", sum(6, 7))
+console.log(">>> sum(6, 7): ", sum(6, 7, true))
+// console.log(">>> sum(): ", sum()) // error
+
+export { }
--- chapter3/current_lesson.js
+++ chapter3/current_lesson.js
@@ -1,8 +1,8 @@
-function sum(a, b, c) {
+function sum(a, b, c = 0) {
     return a + b + c;
 }
 
 console.log(">>> sum(6, 7): ", sum(6, 7))
-console.log(">>> sum(): ", sum())
+console.log(">>> sum(): ", sum()) // error
 
 
