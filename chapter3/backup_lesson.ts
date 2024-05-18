f85568e23b08e398b15fd059d66afde10feb0f23
Tien Nham <tiennham@Tiens-MacBook-Pro.local>
2024-05-18 16:14:01 +0700

    static property and method

--- chapter3/backup_lesson.ts
+++ chapter3/backup_lesson.ts
@@ -565,3 +565,29 @@ console.log(">>> employee.describe", employee.describe())
 
 export { }
 
+// lesson 38: static
+
+class Circle {
+    static pi = 3.14
+    radius: number
+    constructor(radius: number) {
+        this.radius = radius
+    }
+
+    static calculateArea(radius: number) {
+        return this.pi * radius * radius
+    }
+}
+
+console.log(">>> pi: ", Circle.pi)
+
+console.log("Area 10: ", Circle.calculateArea(10))
+
+let circle = new Circle(10)
+
+// console.log(">>> circle 10 pi: ", circle.pi) // error
+// console.log(">>> circle 10 area: ", circle.calculateArea(10)) // error
+
+
+export { }
+
--- chapter3/current_lesson.ts
+++ chapter3/current_lesson.ts
@@ -1,39 +1,25 @@
-// lesson 37: inheritance
+// lesson 38: static
 
-class Person {
-    firstName: string;
-    lastName: string;
-
-    constructor(firstName: string, lastName: string) {
-        this.firstName = firstName
-        this.lastName = lastName
+class Circle {
+    static pi = 3.14
+    radius: number
+    constructor(radius: number) {
+        this.radius = radius
     }
 
-    getFullName() {
-        return `${this.firstName} ${this.lastName}`
-    }
-
-    describe() {
-        return `This is describe`
-    }
-
-}
-
-class Employee extends Person {
-    private jobTitle: string;
-
-    constructor(fristName: string, lastName: string, jobTitle: string) {
-        super(fristName, lastName)
-        this.jobTitle = jobTitle
-    }
-
-    describe(): string {
-        return `${super.describe()} + for Employee class`
+    static calculateArea(radius: number) {
+        return this.pi * radius * radius
     }
 }
 
-let employee = new Employee("Tien", "Nham", "Data Engineering")
-console.log(">>> employee.describe", employee.describe())
+console.log(">>> pi: ", Circle.pi)
+
+console.log("Area 10: ", Circle.calculateArea(10))
+
+let circle = new Circle(10)
+
+// console.log(">>> circle 10 pi: ", circle.pi) // error
+// console.log(">>> circle 10 area: ", circle.calculateArea(10)) // error
 
 
 export { }
